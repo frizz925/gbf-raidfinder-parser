a95ec3c179f0aff74a30eab6562af0ae3360f886
Izra Faturrahman <Frizz925@hotmail.com>
2018-01-29 02:09:36 +0700

    Fixed parser not reading Twitter image

--- src/RaidTweet.ts
+++ src/RaidTweet.ts
@@ -23,6 +23,7 @@ export default class RaidTweet extends ParsedText {
   public static readonly parse = parseTweet;
   public static readonly isRaidTweet = isRaidTweet;
 
+  public image: string;
   public language: string;
   public raid: Raid;
   public boss: Boss;
@@ -32,15 +33,20 @@ export default class RaidTweet extends ParsedText {
       .trim()
       .split('\n')
       .reverse();
-    if (lines.length < 3) {
+    if (lines.length < 4) {
       throw this._invalidFormatError(text);
     }
 
     this.text = text;
-    this.boss = new Boss(lines[0]);
-    this.raid = new Raid(lines[2]);
-    this.language = this.boss.language;
+    this.image = lines[0].trim();
+    this.boss = new Boss(lines[1]);
+    this.language = this.getLanguage(lines[2]);
+    this.raid = new Raid(lines[3]);
     return this;
+  }
+
+  public getLanguage(text: string): string {
+    return text.match(/参加者募集！/) ? 'jp' : 'en';
   }
 
   private _invalidFormatError(text: string): TypeError {
